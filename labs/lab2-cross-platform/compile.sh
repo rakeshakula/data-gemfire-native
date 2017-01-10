8dc90ed1824816721837b0f07518da768f07b476
Eitan Suez <eitan.suez@gmail.com>
2017-01-10 13:15:29 -0600

    reviewed, slightly edited compile command after reviewing docs

--- labs/lab2-cross-platform/compile.sh
+++ labs/lab2-cross-platform/compile.sh
@@ -1,1 +1,2 @@
-g++ -D_REENTRANT -O3 -Wall -m64 -I${GFCPP}/include -L${GFCPP}/lib -Wl,-rpath,${GFCPP}/lib -lgfcppcache PdxInstance.cpp -o PdxInstance
+g++ -D_REENTRANT -O3 -Wall -m64 -I${GFCPP}/include PdxInstance.cpp -o PdxInstance -L${GFCPP}/lib -Wl,-rpath,${GFCPP}/lib -lgfcppcache
+
